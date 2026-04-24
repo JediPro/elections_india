2c80adccaa06336565eb498ecc22fa75d5cc90a7
Pratik C <pratikranit@gmail.com>
2026-04-25 00:27:41 +0530

    process data pt 1

--- west_bengal_vs.R
+++ west_bengal_vs.R
@@ -7,10 +7,34 @@ library(sf)
 # Set working directory ------------------------------
 setwd("C:\\Stuff\\Datasets\\GitHub\\elections_india\\")
 
-# Fetch Assemble constituency shapefile for WB ---------------------
-sf_wb_ac <- st_read(dsn = "state_vs_ac/S25_AC.shp")
+# Load data ------------------------------------
+# Fetch Assemble constituency shapefile for WB
+sf_wb_ac_raw <- st_read(dsn = "state_vs_ac/S25_AC.shp") %>% 
+  st_set_crs(value = 4326)
 
+# Fetch data of past elections from Lok Dhaba https://lokdhaba.ashoka.edu.in/
+table_ac_elections <- read_csv("West_Bengal_AE.csv")
 
+# Process files ------------------------------------
+# Shapefile
+sf_wb_ac <- sf_wb_ac_raw %>% 
+  select(ST_CODE, AC_NO, PC_NO, AC_NAME) %>% 
+  arrange(AC_NO) %>% 
+  rename_with(.fn = snakecase::to_snake_case, .cols = everything())
 
+# Results
+data_wb_ac_elections <- table_ac_elections %>% 
+  # Remove bye-election data
+  drop_na(month) %>% 
+  select(Constituency_No, Constituency_Name, Constituency_Type,  
+         Year, Assembly_No, Electors, Valid_Votes, ENOP,
+         Position, Party, Party_ID, Votes, pid, Candidate, 
+         Sex, Age, Candidate_Type, Contested, No_Terms, 
+         MyNeta_education, TCPD_Prof_Main, Turncoat) %>% 
+  rename_with(.fn = snakecase::to_snake_case, .cols = everything()) %>% 
+  # Rename manually
+  rename(candidate_id = pid) %>% 
+  # Process columns
+  mutate()
 
-
+fgh <- table_ac_elections %>% filter(is.na(Sub_Region))
